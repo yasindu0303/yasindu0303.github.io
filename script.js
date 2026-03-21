8e556bed4b080c6689ff54034be096afe3f5fc03
GihanYasindu <63700555+yasindu0303@users.noreply.github.com>
2026-03-22 01:24:17 +1100

    Add smooth scrolling for navigation links

---
+++ script.js
@@ -0,0 +1,14 @@
+document.querySelectorAll("nav a").forEach(link => {
+
+link.addEventListener("click", function(e){
+
+e.preventDefault();
+
+document.querySelector(this.getAttribute("href"))
+.scrollIntoView({
+behavior:"smooth"
+});
+
+});
+
+});
